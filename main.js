a309d593373a9c22b507ffda232b84898ea0ed4e
CaduDevWeb <carloseduardo.casotti@outlook.com>
2025-09-26 16:59:45 -0300

    estrutura do projeto e comeco do layout

---
+++ main.js
@@ -0,0 +1,18 @@
+const {app, BrowserWindow} = require ('electron')
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height:800,
+        webPreferences: {
+            backgroundColor: '#2c3e50',
+            nodeIntegration: false,
+            contextIsolation: true
+        }
+    })
+    mainWindow.loadFile('src/renderer/index.html')
+    mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
