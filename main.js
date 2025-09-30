8519d3818c204d5841075fc5f4f08baa8ff94111
CaduDevWeb <carloseduardo.casotti@outlook.com>
2025-09-30 18:22:18 -0300

    mais mais mudancas na interface

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const createWindow = () => {
     const mainWindow = new BrowserWindow({
         width: 1200,
         height:800,
+        autoHideMenuBar: false,
         webPreferences: {
             backgroundColor: '#2c3e50',
             nodeIntegration: false,
