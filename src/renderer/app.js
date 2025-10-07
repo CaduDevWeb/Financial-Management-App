a8413c312f89d0575b351aa854784f298cfebe8c
CaduDevWeb <carloseduardo.casotti@outlook.com>
2025-10-06 21:34:36 -0300

    função armazenamento local criada e armazenamento de historico começada

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -2,16 +2,60 @@
 const inputValor = document.getElementById('expense-value')
 const inputCategoryExpense = document.getElementById('expense-category')
 const addExpenseBtn = document.getElementById('add-expense-btn')
+const elementForm = document.getElementById('expense-form')
+const expenseLog = document.getElementById('list-expense')
 
-//Funcoes
-function addExpense(event) {
-    event.preventDefault()
-    const formData = new FormData(myForm);
+//funções
+function addLog() {
+    const dataSavedJSON = localStorage.getItem('DataUsers')
 
-    //Definir como fazer para criar o formData e salva ele 
-    const expenseValue = forn
+    if (!dataSavedJSON) {
+        console.log("nenhum dado encontrado no localStorage")
+        return
+    }
 
+    //Converter de volta a objeto
+    const objectDataReconvert = JSON.parse(dataSavedJSON)
+
+    // criando o elemento da lista
+    const newLi = document.createElement('li');
+    newLi.classList.add('li-expense')
+    const newLink = document.createElement('a')
+    newLink.classList.add('expense-link')
+    const spanValue = document.createElement('span')
+    spanValue.textContent = `R$ ${objectDataReconvert.value}`
+    const spanType = document.createElement('span')
+    spanType.textContent = objectDataReconvert.type
+    
+    //agora colocar em hierarquia
+    expenseLog.appendChild(newLi)
+    newLi.appendChild(newLink)
+    newLink.appendChild(spanValue)
+    newLink.appendChild(spanType)
 }
 
 //Evento de click
-addExpenseBtn.addEventListener('click', addExpense)
+elementForm.addEventListener('submit',function(event) {
+    event.preventDefault()
+    
+    const formElement = event.target;
+    
+    const objectData = {
+        value: formElement.elements['expense-value'].value,
+        type:  formElement.elements['expense-category'].value
+    };
+    
+    //Converter o objeto em json
+    
+    const dataJSON = JSON.stringify(objectData);
+    
+    //Salvar no localStorage
+    
+    localStorage.setItem('DataUsers', dataJSON);
+    
+    console.log('Dados salvos')
+    console.log(dataJSON)
+
+    addLog();
+})
+
